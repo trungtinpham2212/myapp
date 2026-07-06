8c9eb2baa36424b176bbdd14e29885701eba352d
trungtinpham2212 <tt221203@gmail.com>
2026-07-06 14:49:33 +0700

    fix swagger production

--- Repositories/DBContext/myappContext.cs
+++ Repositories/DBContext/myappContext.cs
@@ -42,9 +42,9 @@ public partial class myappContext : DbContext
 
     public virtual DbSet<Wishlist> Wishlists { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Username=postgres;Password=111111;Host=localhost;Database=myapp");
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//         => optionsBuilder.UseNpgsql("Username=postgres;Password=111111;Host=localhost;Database=myapp");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
